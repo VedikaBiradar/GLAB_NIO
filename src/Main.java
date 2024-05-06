acc60bae4b3f5c1f6fbc9c0f37ab02d592ae9658
VedikaBiradar <vedikabiradar2001@gmail.com>
2024-05-06 18:21:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//      NioEXample no = new NioEXample();
+//      no.NioExample();
+
+        NioExampleTwo nt = new NioExampleTwo();
+        nt.nioExample2();
+
+    }
+}
